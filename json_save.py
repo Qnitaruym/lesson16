b60ceba49ef22297ae50721693d08b12427836c2
Egor <miksfifs@gmail.cpm>
2024-12-26 18:16:21 +0300

    h

--- json_save.py
+++ json_save.py
@@ -1,9 +1,17 @@
-import json
-auto = {
-    'car': 'Kia',
-    'stamp': 'Seltos',
-    'state_number': 42442
-}
-result = json.dumps(auto)
-print(result)
-print(type(result))
+# import json
+#    auto = {
+#        'car': 'Kia',
+#        'stamp': 'Seltos',
+#        'state_number': 42442
+#   }
+#   result = json.dumps(auto)
+#    print(result)
+#   print(type(result))
+#
+import random
+
+car = ['Kia', 'Renoalt', 'Lada', 'Ford', 'BMW']
+'''for i in range(3):
+    car.append(random.randint(0, 5))
+print(car)'''
+print('Марка Ford есть в нашем списке') if 'Ford' in car else print('Марки Ford нет в нашем списке')
---
+++ triple_operator_01.py
@@ -0,0 +1,6 @@
+result = None
+if 4 in numbers:
+    result = 'Есть  китайское несчасливое число'
+else:
+    result = 'Есть  китайское несчасливое число' if 4 in numbers else 'Все окей'
+    print('Результат выражения:\n', result)
